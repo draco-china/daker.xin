9da771a8f4ee5ba81e7f73f6c6b8cfe74d2bfd1f
Daker Zhou <Daker.Zhou@gmail.com>
2017-12-28 10:30:23 +0800

    增加百度统计

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,12 @@ module.exports = {
     ],
     script: [
       {
+        async: 'async',
+        defer: 'defer',
+        tyep: 'text/javascript',
+        innerHTML: `var _hmt = _hmt || []; (function() { var hm = document.createElement("script"); hm.src = "https://hm.baidu.com/hm.js?6c9dda94741a5414ed128012ea1e9b31"; var s = document.getElementsByTagName("script")[0];  s.parentNode.insertBefore(hm, s);})();`
+      },
+      {
         // async: 'async',
         // defer: 'defer',
         // type: 'text/javascript',
